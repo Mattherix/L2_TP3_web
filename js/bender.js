67749c19e96530cdc1f991838fad9ab0abc24d1a
mattherix <mattherix@protonmail.com>
2022-05-23 16:34:40 +0200

    feat: robot movements

--- js/bender.js
+++ js/bender.js
@@ -1,5 +1,6 @@
 function create_robot(){
   let bender = document.createElement('img');
+  bender.id = 'bender';
   bender.src = "../img/Bender.png";
   bender.style.height = "100px";
   bender.style.position = "absolute";
@@ -8,4 +9,21 @@ function create_robot(){
   bender.style.transition = "all 2s";
   bender.id = "bender";
   document.body.append(bender);
+}
+
+function move_bot_right() {
+  let bender = document.getElementById('bender');
+  bender.style.left = (parseInt(bender.style.left) + 100) + 'px';
+}
+function move_bot_left() {
+  let bender = document.getElementById('bender');
+  bender.style.left = (parseInt(bender.style.left) - 100) + 'px';
+}
+function move_bot_top() {
+  let bender = document.getElementById('bender');
+  bender.style.top = (parseInt(bender.style.top) - 100) + 'px';
+}
+function move_bot_bottom() {
+  let bender = document.getElementById('bender');
+  bender.style.top = (parseInt(bender.style.top) + 100) + 'px';
 }
